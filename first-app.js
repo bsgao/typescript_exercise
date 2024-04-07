fb264a033746f6719ad863bd5032ae462c9e3a24
bsgao <brandongao2003@gmail.com>
2024-04-07 00:03:38 -0400

    changed the type of userID

---
+++ first-app.js
@@ -0,0 +1,4 @@
+var userName = "Max";
+userName = "Max";
+var userID = "abc1";
+userID = 123;
---
+++ first-app.ts
@@ -0,0 +1,5 @@
+let userName: string = "Max";
+userName = "Max";
+
+let userID: string | number = "abc1";
+userID = 123;
